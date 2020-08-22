d8271120a08678c6286de87715780d70c17b2bac
yxl <18217692140@163.COM>
2020-08-22 22:55:41 +0800

    微服务模块4

---
+++ cloud-comsumer-order-80/src/main/java/com/yxl/ribbon/MySelfRule.java
@@ -0,0 +1,15 @@
+package com.yxl.ribbon;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();    //负载均衡机制改为随机
+    }
+}
