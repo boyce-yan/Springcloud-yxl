17fc59d2ae361d99b155cb71bbe4a563ceb165f3
yxl <18217692140@163.COM>
2020-09-16 09:11:30 +0800

    微服务模块18

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/yxl/cloud/controller/FlowLimitController.java
@@ -0,0 +1,18 @@
+package com.yxl.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "----testB";
+    }
+}
